411a40a2b269c6063f7a23711b6428c604f9185a
Keith Clarke <000728303@tarleton.edu>
2021-09-14 08:38:06 -0500

    QueueException added

---
+++ src/main/java/com/mycompany/datastructures/QueueException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.datastructures;
+
+/**
+ *
+ * @author 000728303
+ */
+public class QueueException extends RuntimeException{
+
+    public QueueException(String message) {
+        super(message);
+    }
+}
